f3e0e0b6dcd6905866c40d9712ba8ebffcd311f1
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-31 23:59:56 +0000

    changes to singular files

--- prolog-programming/2e_addup.pl
+++ prolog-programming/2e_addup.pl
@@ -1,3 +1,4 @@
 addUpList([],[]).
+addUpList([X],[X]).
 /*addUpList([X],[X]) :- addUpList(X,_).*/
-addUpList([X,Y|L1],[K|L2]) :- K is X+Y, addUpList(L1,L2).
+/*addUpList([X,Y|L1],[K|L2]) :- K is X+Y, addUpList(L1,L2).*/
--- prolog-programming/3a_mymerge.pl
+++ prolog-programming/3a_mymerge.pl
@@ -1,4 +1,5 @@
 mymerge([], [], []).
 mymerge(X, [], X).
 mymerge([], X, X).
-mymerge([HX|X], [HY|Y], [HX,HY|Z]) :- !, mymerge(X, Y, Z).
+mymerge([HX|X], [HY|Y], [HX|Z]):- HX < HY, mymerge(X, [HY|Y], Z).
+mymerge([HX|X], [HY|Y], [HY|Z]) :- HX >= HY, mymerge([HX|X], Y, Z).
