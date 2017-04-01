9d1d11bc14a7dec1c4ab165485108f727dc706ec
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-04-01 00:20:27 +0000

    temp remove element file updated

--- prolog-programming/2b_removeelement.pl
+++ prolog-programming/2b_removeelement.pl
@@ -6,6 +6,12 @@ removeElement(_,[H|L1],[L1|L2]) :- removeElement(_,L1,L2).*/
 /*removeElement(X,[H|L1],[H|L2]) :- removeElement(X,L1,L2).*/
 
 removeElement(X,[X],[]).
+removeElement(X,[X|L1],L1).
+removeElement(X,[Y|L1],[Y|L2]):- removeElement(X,L1,L2).
 /*removeElement(X,[X|T],L) :- removeElement(X,T,L).*/
 
 /*removeElement(X,[_|T],L) :- removeElement(X,T,L).*/
+/*removeElement(_,[],[]).
+removeElement(X,[X],[]).
+removeElement(X,[X|L1],L1).
+removeElement(X,[Y|L1],[Y|L2]):- removeElement(X,L1,L2).*/
